66674b700342adb35a6ae10e61b75d6121f23767
asakeaoyama <aoyama07240316@gmail.com>
2021-06-01 14:38:53 +0800

    add all struction

---
+++ ttd/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render
+
+from ttd.models import *
+
+# Create your views here.
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import *
+
+from datetime import datetime
+
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
+
+
+@csrf_exempt
+def homepage(request):
+    return render(request, 'home.html',{'current_time': str(datetime.now()),})
+
+
+@csrf_exempt
+def pic(request):
+    return render(request, 'surprise.html')
+
+
+
+@csrf_exempt
+def callback(request):
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+
+        try:
+            events = parser.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+
+        for event in events:
+            if isinstance(event, MessageEvent):
+                if event.message.type=='text':
+                    mtext=event.message.text
+                    uid=event.source.user_id
+                    profile=line_bot_api.get_profile(uid)
+                    name=profile.display_name
+                    pic_url=profile.picture_url
+                    message=[]
+
+                    if event.message.text == '建立會員資料':
+                        if User_Info.objects.filter(uid=uid).exists()==False:
+                            User_Info.objects.create(uid=uid,name=name,pic_url=pic_url,mtext=mtext)
+                            message.append(TextSendMessage(text='會員資料新增完畢'))
+                        elif User_Info.objects.filter(uid=uid).exists()==True:
+                            message.append(TextSendMessage(text='已經有建立會員資料囉'))
+                            user_info = User_Info.objects.filter(uid=uid)
+                            for user in user_info:
+                                info = 'UID=%s\nNAME=%s\n大頭貼=%s'%(user.uid,user.name,user.pic_url)
+                                message.append(TextSendMessage(text=info))
+                        line_bot_api.reply_message(event.reply_token,message)
+                    elif event.message.text == '你好胖':
+                        message.append(TextSendMessage(text='你媽才胖'))
+                        line_bot_api.reply_message(event.reply_token,message)
+                    elif event.message.text == '51d帥照' :
+                        line_bot_api.reply_message(event.reply_token,ImageSendMessage(original_content_url='https://spacetotest.herokuapp.com/static/kai.jpg', preview_image_url='https://spacetotest.herokuapp.com/static/kai.jpg'))
+                        
+
+
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
