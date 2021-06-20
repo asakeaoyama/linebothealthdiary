c182e5fbe34a8df870ea1bd0a4866ff47507331e
asakeaoyama <aoyama07240316@gmail.com>
2021-06-20 10:24:19 +0800

    fix

--- ttd/views.py
+++ ttd/views.py
@@ -112,9 +112,9 @@ def callback(request):
                         f.close()
                     
                     elif mes[0] == 'bmi':
-                        highth=float(float(mes[1])/100)
-                        weight=float(mes[2])
-                        bmi=float(weight/((highth)^2))
+                        #highth=float(float(mes[1])/100)
+                        #weight=float(mes[2])
+                        #bmi=float(weight/((highth)^2))
                         message.append(TextSendMessage(text=mes[1]))
                         line_bot_api.reply_message(event.reply_token,message)
 
